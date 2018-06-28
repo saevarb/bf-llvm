f1796ac7c3cbb33124c8c9be0ee9ebd793610640
Sævar Berg <icedane@gmail.com>
2018-06-28 17:52:05 +0200

    Some limited jit stuff seems to work

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,12 @@ import LLVM.IRBuilder.Module
 import LLVM.IRBuilder.Monad
 import LLVM.IRBuilder.Instruction
 import LLVM.AST.IntegerPredicate
+import LLVM.OrcJIT
+import LLVM.Target
+import LLVM.ExecutionEngine as EE
+import Data.Int
+import Foreign.Ptr
+import qualified Data.ByteString as BS
 
 import Lib
 import Parser
@@ -40,42 +46,75 @@ type MyMonad = ReaderT Environment IRBuilder
 -- newtype MyMonad a = MyMonad (Reader Environment a)
 --     deriving (Functor, Applicative, Monad, MonadReader Environment)
 
+foreign import ccall "dynamic" haskFun :: FunPtr (IO Int8) -> (IO Int8)
+
+run :: FunPtr a -> IO Int8
+run fn = haskFun (castFunPtr fn :: FunPtr (IO Int8))
+
+runJIT :: AST.Module -> IO ()
+runJIT mod = do
+    withContext $ \ctx ->
+        withModuleFromAST ctx mod $ \m ->
+        jit ctx $ \executionEngine ->
+        EE.withModuleInEngine executionEngine m $ \ee -> do
+        mainfn <- EE.getFunction ee (AST.Name "main")
+        case mainfn of
+            Just fn -> do
+                res <- run fn
+                -- putStrLn $ "Evaluated to: " ++ show res
+                return ()
+            Nothing -> return ()
+
+jit :: Context -> (EE.MCJIT -> IO a) -> IO a
+jit c = EE.withMCJIT c optlevel model ptrelim fastins
+  where
+    optlevel = Just 2  -- optimization level
+    model    = Nothing -- code model ( Default )
+    ptrelim  = Nothing -- frame pointer elimination
+    fastins  = Nothing -- fast instruction selection
+
 
 emit :: (MonadFix m, MonadIRBuilder m) => Operand -> BrainfuckOp -> m ()
-emit ptr IncPtr =
-    gep ptr [ConstantOperand (Int 8 1)] >>= store ptr 0
-emit ptr DecPtr =
-    gep ptr [ConstantOperand (Int 8 (-1))] >>= store ptr 0
-emit ptr IncVal = do
-    val <- load ptr 0
-    add val (ConstantOperand (Int 8 1)) >>= store val 0
-emit ptr DecVal = do
-    val <- load ptr 0
-    sub val (ConstantOperand (Int 8 1)) >>= store val 0
-emit ptr (Loop ops) = mdo
+emit buf IncPtr =
+    gep buf [ConstantOperand (Int 8 1)] >>= store buf 0
+emit buf DecPtr =
+    gep buf [ConstantOperand (Int 8 (-1))] >>= store buf 0
+emit buf IncVal = do
+    val <- load buf 0
+    add val (ConstantOperand (Int 8 1)) >>= store buf 0
+emit buf DecVal = do
+    val <- load buf 0
+    sub val (ConstantOperand (Int 8 1)) >>= store buf 0
+emit buf (Loop ops) = mdo
     start <- block `named` "start"
-    val <- load ptr 0
+    mapM_ (emit buf) ops
+    val <- load buf 0
     test <- icmp NE val (ConstantOperand (Int 8 0))
     condBr test start exit
     exit <- block `named` "exit"
     return ()
-emit ptr WriteVal = return ()
-emit ptr ReadVal = return ()
+emit buf WriteVal = do
+    val <- load buf 0
+    -- call putch [(val, [])]
+    return ()
+emit buf ReadVal = return ()
 
 main :: IO ()
 main = do
     withContext $ \ctx -> do
       withModuleFromAST ctx myModule $ \module' -> do
-        moduleLLVMAssembly module' >>= pPrint
+        moduleLLVMAssembly module' >>= BS.putStrLn
+    runJIT myModule
     return ()
   where
-    parsed = fromRight [] $ parse brainfuckP "stdin" helloWorldNoComments
-    -- parsed = fromRight [] $ parse brainfuckP "stdin" "[+][-]"
+    -- parsed = fromRight [] $ parse brainfuckP "stdin" helloWorldNoComments
+    parsed = fromRight [] $ parse brainfuckP "stdin" ">>>>>>"
     myModule =
         buildModule "testModule" $ do
-            -- emitDefn (GlobalDefinition buffer)
-            -- emitDefn (GlobalDefinition bufPtr)
-            function "main" [] void $ \_ -> do
-                buf <- alloca i8 (Just $ ConstantOperand (Int 32 32768)) 0 `named` "buffer"
-                mapM (emit buf) parsed
-                retVoid
+        memset <- extern (Name "memset") [PointerType i8 (AddrSpace 0), i8, i32] void
+        function "main" [] i8 $ \_ -> do
+            buf <- alloca i8 (Just $ ConstantOperand (Int 32 32768)) 0 `named` "buffer"
+            call memset [(buf, []), (ConstantOperand (Int 8 0), []), (ConstantOperand (Int 32 32768), [])]
+            mapM_ (emit buf) parsed
+            end <- load buf 0
+            ret end
