8ff61c8431f5773e095067c525d08115f7ae3f9e
Sævar Berg <icedane@gmail.com>
2018-06-28 20:06:06 +0200

    Things mostly seem to work

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,8 @@ module Main where
 import Data.Either
 import Control.Monad.Reader hiding (void)
 
-import Text.Megaparsec
-import Text.Megaparsec.Error
+import Text.Megaparsec (parse)
+-- import Text.Megaparsec.Error
 import Text.Pretty.Simple
 
 import LLVM.Module
@@ -17,19 +17,22 @@ import LLVM.AST.Type
 import LLVM.AST.AddrSpace
 import LLVM.AST.Constant as C
 import LLVM.Internal.Context
-import qualified LLVM.AST.Global as G
 import LLVM.IRBuilder.Module
 import LLVM.IRBuilder.Monad
 import LLVM.IRBuilder.Instruction
 import LLVM.AST.IntegerPredicate
-import LLVM.OrcJIT
-import LLVM.Target
+import LLVM.Analysis
+-- import qualified LLVM.AST.Global as G
+-- import LLVM.OrcJIT
+-- import LLVM.Target
 import LLVM.ExecutionEngine as EE
 import Data.Int
 import Foreign.Ptr
 import qualified Data.ByteString as BS
+import Control.Exception
+import LLVM.Exception
+import System.Environment
 
-import Lib
 import Parser
 
 helloWorldNoComments :: String
@@ -40,6 +43,7 @@ data Environment =
     Environment
     { bufRef :: Operand
     , idxRef :: Operand
+    , putchRef :: Operand
     } deriving (Show)
 
 -- type MyMonad = IRBuilderT (Reader Environment)
@@ -62,7 +66,7 @@ runJIT mod = do
         case mainfn of
             Just fn -> do
                 res <- run fn
-                putStrLn $ "Evaluated to: " ++ show res
+                putStrLn $ "Result of JIT: " ++ show res
                 return ()
             Nothing -> return ()
 
@@ -78,59 +82,78 @@ jit c = EE.withMCJIT c optlevel model ptrelim fastins
 emit :: (MonadReader Environment m, MonadFix m, MonadIRBuilder m) => BrainfuckOp -> m ()
 emit IncPtr = do
     idx <- asks idxRef
-    load idx 0 >>= add (ConstantOperand (Int 8 1)) >>= store idx 0
+    load idx 0 >>= add (ConstantOperand (Int 32 1)) >>= store idx 0
 emit  DecPtr = do
     idx <- asks idxRef
-    load idx 0 >>= sub (ConstantOperand (Int 8 1)) >>= store idx 0
+    load idx 0 >>= sub (ConstantOperand (Int 32 1)) >>= store idx 0
 emit  IncVal = do
     buf <- asks bufRef
     idxr <- asks idxRef
     ival <- load idxr 0
     pos <- gep buf [ival]
     val <- load pos 0
-    add val (ConstantOperand (Int 8 1)) >>= store buf 0
+    add val (ConstantOperand (Int 8 1)) >>= store pos 0
 emit  DecVal = do
     buf <- asks bufRef
     idxr <- asks idxRef
     ival <- load idxr 0
     pos <- gep buf [ival]
     val <- load pos 0
-    sub val (ConstantOperand (Int 8 1)) >>= store buf 0
+    sub val (ConstantOperand (Int 8 1)) >>= store pos 0
 emit  (Loop ops) = mdo
     buf <- asks bufRef
+    idxr <- asks idxRef
+    br start
     start <- block `named` "start"
-    mapM_ (emit ) ops
-    val <- load buf 0
-    test <- icmp NE val (ConstantOperand (Int 8 0))
+    mapM_ emit  ops
+    ival <- load idxr 0
+    pos <- gep buf [ival]
+    val <- load pos 0
+    test <- icmp UGT val (ConstantOperand (Int 8 0))
     condBr test start exit
     exit <- block `named` "exit"
     return ()
 emit  WriteVal = do
     buf <- asks bufRef
-    val <- load buf 0
-    -- call putch [(val, [])]
+    idx <- asks idxRef
+    val <- load idx 0
+    pos <- gep buf [val]
+    val <- load pos 0
+    putch <- asks putchRef
+    call putch [(val, [])]
     return ()
 emit  ReadVal = return ()
 
 main :: IO ()
 main = do
+    [program] <- getArgs
+    let parsed = fromRight [] $ parse brainfuckP "stdin" program
     withContext $ \ctx -> do
-      withModuleFromAST ctx myModule $ \module' -> do
-        moduleLLVMAssembly module' >>= BS.putStrLn
-    runJIT myModule
-    return ()
+        withModuleFromAST ctx (myModule parsed) $ \module' -> do
+            result <- try (verify module')
+            moduleLLVMAssembly module' >>= BS.putStrLn
+            case result of
+                Left e -> handler e
+                Right _ -> do
+                    runJIT (myModule parsed)
+        return ()
   where
+    handler :: VerifyException -> IO ()
+    handler (VerifyException e) = putStrLn e
     -- parsed = fromRight [] $ parse brainfuckP "stdin" helloWorldNoComments
-    parsed = fromRight [] $ parse brainfuckP "stdin" "---++++>><<>>-++-"
-    myModule =
+    parsed = fromRight [] $ parse brainfuckP "stdin" "++++[>++<-]>>"
+    myModule program =
         buildModule "testModule" $ do
         memset <- extern (Name "memset") [PointerType i8 (AddrSpace 0), i8, i32] void
+        putch <- extern (Name "putchar") [i8] void
         function "main" [] i8 $ \_ -> do
             buf <- alloca i8 (Just $ ConstantOperand (Int 32 32768)) 0 `named` "buffer"
             idx <- alloca i32 (Just $ ConstantOperand (Int 32 1)) 0 `named` "index"
             store idx 0 (ConstantOperand (Int 32 0))
-            -- call memset [(buf, []), (ConstantOperand (Int 8 0), []), (ConstantOperand (Int 32 32768), [])]
-            let env = Environment buf idx
-            runReaderT (mapM_ emit parsed) env
-            end <- load buf 0
+            call memset [(buf, []), (ConstantOperand (Int 8 0), []), (ConstantOperand (Int 32 32768), [])]
+            let env = Environment buf idx putch
+            runReaderT (mapM_ emit program) env
+            ival <- load idx 0
+            pos <- gep buf [ival]
+            end <- load pos 0
             ret end
